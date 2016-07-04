84d9b28687e1f1451bddca11ff95a057eb5ff194
xulan <xulan@chinawiserv.com>
2016-07-04 11:24:02 +0800

    Add Logout

--- client/scripts/common/main.js
+++ client/scripts/common/main.js
@@ -2,8 +2,33 @@
 var Main = angular.module('Main', ['ui.router', 'ngCookies']);
 
 /** Main Controller */
-Main.controller('Main.Controller.Main', ['$scope', '$cookies',
-  function($scope, $cookies) {
+Main.controller('Main.Controller.Main', ['$scope', '$cookies', 'Main.Service.Http', '$state',
+  function($scope, $cookies, Http, $state) {
     $scope.User = JSON.parse($cookies.get('User'));
+    $scope.logout = function() {
+      console.log($scope.User);
+      if(sessionStorage.token){
+        sessionStorage.removeItem('token');
+        Http.logout().then(function(result) {
+          $state.go("welcome");
+        })
+      }
+
+    }
+  }
+]);
+
+/* HTTP Factory */
+Main.factory('Main.Service.Http', ['$http', 'API',
+  function($http, API) {
+    var path = API.path;
+    function logout() {
+      return $http.get(
+        path + '/loginOut'
+      )
+    };
+    return {
+      logout: logout
+    }
   }
 ]);
