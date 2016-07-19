e874e74100452e05287a5881c053e22b49576192
xulan <xulan@chinawiserv.com>
2016-07-19 17:04:51 +0800

    Add download help doc

--- client/scripts/common/main.js
+++ client/scripts/common/main.js
@@ -2,9 +2,10 @@
 var Main = angular.module('Main', ['ui.router', 'ngCookies']);
 
 /** Main Controller */
-Main.controller('Main.Controller.Main', ['$scope', '$cookies', 'Main.Service.Http', '$state',
-  function($scope, $cookies, Http, $state) {
+Main.controller('Main.Controller.Main', ['$scope', '$cookies', 'Main.Service.Http', '$state', 'API',
+  function($scope, $cookies, Http, $state, API) {
     $scope.User = JSON.parse($cookies.get('User'));
+    $scope.downloadHelp = API.path + '/download?help=zip';
     $scope.logout = function() {
       console.log($scope.User);
       if(sessionStorage.token){
